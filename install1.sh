029d4760b9dac357f6d580816f23ad7d495e9304
og900aero <49487376+og900aero@users.noreply.github.com>
2022-09-15 16:55:45 +0200

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -7,3 +7,58 @@ EOF
 
 wget -O- https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/google.gpg
 apt update
+
+mkdir -p /home/Data
+chmod 744 /home/Data
+sed -i 's/errors=remount-ro/defaults,relatime/g' /mnt/etc/fstab
+echo "/dev/sda3 /home/Data ext4 defaults,relatime 0 2" >> /etc/fstab
+
+echo "vm.swappiness=10" >> /etc/sysctl.d/local.conf
+
+cat <<EOF > /etc/X11/xorg.conf.d/20-intel.conf
+Section "Device"
+    Identifier "Intel Graphics"
+    Driver "intel"
+    Option "Backlight" "intel_backlight"
+    Option "AccelMethod" "sna"
+    Option "TearFree" "true"
+    Option "TripleBuffer" "true"
+EndSection
+EOF
+
+echo "MaxRetentionSec=15day" >> /etc/systemd/journald.conf"
+
+cat > /etc/X11/xorg.conf.d/40-libinput.conf <<EOF
+Section "InputClass"
+        Identifier "libinput pointer catchall"
+        MatchIsPointer "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+EndSection
+Section "InputClass"
+        Identifier "libinput keyboard catchall"
+        MatchIsKeyboard "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+EndSection
+Section "InputClass"
+        Identifier "libinput touchpad catchall"
+        MatchIsTouchpad "on"
+        Option "Tapping" "on"
+        Option "TappingButtonMap" "lmr"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+EndSection
+Section "InputClass"
+        Identifier "libinput touchscreen catchall"
+        MatchIsTouchscreen "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+EndSection
+Section "InputClass"
+        Identifier "libinput tablet catchall"
+        MatchIsTablet "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+EndSection
+EOF
