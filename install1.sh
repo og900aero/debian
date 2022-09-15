b6defec4ae8c72272657009f9992deef482e6a11
og900aero <49487376+og900aero@users.noreply.github.com>
2022-09-15 08:40:35 +0200

    Create install1.sh

---
+++ install1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo -i
+
+cat << EOF > /etc/apt/sources.list.d/google-chrome.list
+deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
+EOF
+
+wget -O- https://dl.google.com/linux/linux_signing_key.pub |gpg --dearmor > /etc/apt/trusted.gpg.d/google.gpg
+apt update
