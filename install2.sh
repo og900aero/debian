002bd53f1b041e9792c9937106d91aa9554f3fe5
og900aero <49487376+og900aero@users.noreply.github.com>
2022-09-15 15:44:13 +0200

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt install imagemagick imv trash-cli xclip fuse-zip zip unzip unrar curlftpfs fzf mediainfo ueberzug bspwm sxhkd exa subversion i3lock xautolock dunst kitty libreoffice-gtk3 libreoffice-l10n-hu transmission-gtk gnome-calculator vifm neofetch mpv grsync htop sshfs rofi
-apt install go-mtpfs archivemount google-chrome-stable polybar inxi micro network-manager network-manager-openvpn build-essential libnotify-bin git rsync cmake xorg xserver-xorg-video-intel xserver-xorg-video-nouveau xserver-xorg-core xserver-xorg-input-synaptics xserver-xorg-input-mouse xserver-xorg-input-libinput xserver-xorg-input-kbd xinit xfonts-encodings va-driver-all intel-microcode
+apt install go-mtpfs archivemount google-chrome-stable polybar inxi micro network-manager network-manager-gnome build-essential libnotify-bin git rsync cmake xorg xserver-xorg-video-intel xserver-xorg-video-nouveau xserver-xorg-core xserver-xorg-input-synaptics xserver-xorg-input-mouse xserver-xorg-input-libinput xserver-xorg-input-kbd xinit xfonts-encodings va-driver-all intel-microcode
 apt install pulseaudio pavucontrol pulseaudio-module-bluetooth blueman
 apt install libmtp-common psmisc xdg-user-dirs wget traceroute man-db bash-completion adb fastboot dbus-x11 fonts-font-awesome fonts-hack-ttf fonts-ubuntu fonts-roboto fonts-dejavu ntfs-3g gnome-keyring policykit-1-gnome xbacklight ffmpeg
 
