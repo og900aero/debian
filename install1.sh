04416a1cbe94e2eec7774f8abee1c9a291da719e
og900aero <49487376+og900aero@users.noreply.github.com>
2022-09-15 17:18:55 +0200

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -13,6 +13,8 @@ apt update
 
 mkdir -p /home/Data
 chmod 744 /home/Data
+chown shyciii:users /home/Data
+
 sed -i 's/errors=remount-ro/defaults,relatime/g' /mnt/etc/fstab
 echo "/dev/sda3 /home/Data ext4 defaults,relatime 0 2" >> /etc/fstab
 
@@ -29,8 +31,11 @@ Section "Device"
 EndSection
 EOF
 
-echo "MaxRetentionSec=15day" >> /etc/systemd/journald.conf"
+cd /home/Data/Linux/Compile/automount-usb
+sudo sh configure.sh
 
+echo "MaxRetentionSec=15day" >> /etc/systemd/journald.conf"
+tar -xvf /home/Data/Linux/Backup/home_backup_debian.tar.zst --directory /home/shyciii
 cat > /etc/X11/xorg.conf.d/40-libinput.conf <<EOF
 Section "InputClass"
         Identifier "libinput pointer catchall"
@@ -65,3 +70,8 @@ Section "InputClass"
         Driver "libinput"
 EndSection
 EOF
+
+sed -i 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=5s/' /etc/systemd/system.conf
+sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
+sed -i 's/#HandleLidSwitchExternalPower=suspend/HandleLidSwitchExternalPower=ignore/' /etc/systemd/logind.conf
+sed -i 's/#HandleLidSwitchDocked=ignore/HandleLidSwitchDocked=ignore/' /etc/systemd/logind.conf
