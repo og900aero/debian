2b1f199ea13229084566be0a4d3939f0875c57f7
og900aero <49487376+og900aero@users.noreply.github.com>
2022-09-16 09:42:16 +0200

    Update install1.sh

--- install1.sh
+++ install1.sh
@@ -3,23 +3,27 @@
 apt update; apt install sudo
 usermod -aG sudo shyciii
 
+# Chrome telepítéshez szükséges csomaglista létrehozása
 apt install gpg -y
 cat << EOF > /etc/apt/sources.list.d/google-chrome.list
 deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
 EOF
-
 wget -O- https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor > /etc/apt/trusted.gpg.d/google.gpg
 apt update
 
+# Adat partícióm felmountlása, jogosultság beállítások
 mkdir -p /home/Data
 chmod 744 /home/Data
 chown shyciii:users /home/Data
 
+# Fstab módosítások
 sed -i 's/errors=remount-ro/defaults,relatime/g' /mnt/etc/fstab
 echo "/dev/sda3 /home/Data ext4 defaults,relatime 0 2" >> /etc/fstab
 
+# Swap file létrehozása
 echo "vm.swappiness=10" >> /etc/sysctl.d/local.conf
 
+# Intel driver beállítása
 cat <<EOF > /etc/X11/xorg.conf.d/20-intel.conf
 Section "Device"
     Identifier "Intel Graphics"
@@ -31,11 +35,14 @@ Section "Device"
 EndSection
 EOF
 
+# USB Driveok automountja
 cd /home/Data/Linux/Compile/automount-usb
-sudo sh configure.sh
+sh configure.sh
 
+# Naplózás beállítása
 echo "MaxRetentionSec=15day" >> /etc/systemd/journald.conf"
-tar -xvf /home/Data/Linux/Backup/home_backup_debian.tar.zst --directory /home/shyciii
+
+# Notebook-hoz doube tap beállítása
 cat > /etc/X11/xorg.conf.d/40-libinput.conf <<EOF
 Section "InputClass"
         Identifier "libinput pointer catchall"
@@ -71,7 +78,19 @@ Section "InputClass"
 EndSection
 EOF
 
+# Timeout beállítása
 sed -i 's/#DefaultTimeoutStopSec=90s/DefaultTimeoutStopSec=5s/' /etc/systemd/system.conf
+
+# Notebook fedeléhez kapcsolódó események beállítása
 sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
 sed -i 's/#HandleLidSwitchExternalPower=suspend/HandleLidSwitchExternalPower=ignore/' /etc/systemd/logind.conf
 sed -i 's/#HandleLidSwitchDocked=ignore/HandleLidSwitchDocked=ignore/' /etc/systemd/logind.conf
+
+# Suckless Terminal telepítése
+cd /home/Data/Linux/Compile/st-0.8.5-debian
+make clean install
+
+# Saját config fileok visszaállítása
+mkdir -p /home/shyciii/mnt/android /home/shyciii/mnt/ftp /home/shyciii/mnt/ssh
+tar -xvf /home/Data/Linux/Backup/home_backup_debian.tar.zst --directory /home/shyciii
+chown -R shyciii:users /home/shyciii/
