7989069ea1a83f2ff0b17372176ca498d1416f65
og900aero <49487376+og900aero@users.noreply.github.com>
2023-03-26 18:45:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,9 @@ apt install -y pulseaudio pavucontrol
 # Ablakezelő szoftver és kiegészítései
 apt install -y bspwm sxhkd i3lock xautolock xclip rofi polybar dunst libnotify-bin
 
+# DWM-nek
+apt install -y libx11-xcb-dev libxcb-res0-dev feh nitrogen
+
 # Fontok
 apt install -y fonts-font-awesome fonts-hack-ttf fonts-ubuntu fonts-roboto fonts-dejavu
 
