308b62402c9cfe2bd1c25d2ee53bd0952e0f03e4
og900aero <49487376+og900aero@users.noreply.github.com>
2023-03-26 18:48:07 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ apt install -y pulseaudio pavucontrol
 apt install -y bspwm sxhkd i3lock xautolock xclip rofi polybar dunst libnotify-bin
 
 # DWM-nek
-apt install -y libx11-xcb-dev libxcb-res0-dev feh nitrogen
+apt install -y libx11-xcb-dev libxcb-res0-dev libxinerama-dev
 
 # Fontok
 apt install -y fonts-font-awesome fonts-hack-ttf fonts-ubuntu fonts-roboto fonts-dejavu
