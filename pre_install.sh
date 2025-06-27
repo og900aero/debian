8fd94602e4c01b6fef144300f32378b643e84e44
og900aero <49487376+og900aero@users.noreply.github.com>
2025-06-27 13:47:47 +0200

    Update pre_install.sh

--- pre_install.sh
+++ pre_install.sh
@@ -2,12 +2,12 @@
 
 ifdown wlp0s20f3
 head -n -5 /etc/network/interfaces > tmp.txt && mv tmp.txt /etc/network/interfaces
+ip link set dev wlp0s20f3 down
 ip link set dev wlp0s20f3 up
 iwlist wlp0s20f3 scan | grep ESSID
 read -p "Add meg a Wi-Fi ESSID-t: " ESSID
 read -s -p "Add meg a Wi-Fi jelszót: " PASS
 echo
-wpa_passphrase "$ESSID" "$PASS" | sudo tee /etc/wpa_supplicant.conf > /dev/null
+wpa_passphrase "$ESSID" "$PASS" | tee /etc/wpa_supplicant.conf > /dev/null
 wpa_supplicant -B -c /etc/wpa_supplicant.conf -i wlp0s20f3
 dhclient wlp0s20f3
-apt install -y git curl
