83acc56702807c83fcc9b28d78f3d24514d5a125
og900aero <49487376+og900aero@users.noreply.github.com>
2025-08-11 08:12:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ apt install -y fonts-font-awesome fonts-dejavu ttf-mscorefonts-installer
 apt install -y unrar-free libfuse3-3 ifuse sshfs mediainfo zip unzip zstd 7zip poppler-utils ffmpegthumbnailer xlsx2csv bat catdoc docx2txt jq libimage-exiftool-perl w3m
 
 # Other programs
-apt install -y imagemagick libreoffice libreoffice-l10n-hu transmission-gtk gnome-calculator mpv rsync grsync btop inxi ffmpeg ncdu
+apt install -y imagemagick libreoffice libreoffice-l10n-hu transmission-gtk gnome-calculator mpv rsync grsync btop inxi ffmpeg ncdu eza zoxide
 
 # For compiles
 apt install -y libxft-dev build-essential cmake make pkg-config fontconfig libxinerama-dev libxcb-res0 libimlib2-dev libdbus-1-dev libx11-xcb-dev libxcb-res0-dev libyajl-dev libevent-dev bison ncurses-dev libcurl3-nss libxcb-util-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-cursor-dev libxcb-xinerama0-dev automake 
@@ -53,9 +53,9 @@ apt install -y apt-file testdisk duf tldr ripgrep xdotool pmount freerdp2-x11 li
 apt-file update
 
 # Install zoxide
-wget https://github.com/ajeetdsouza/zoxide/releases/download/v0.9.8/zoxide_0.9.8-1_amd64.deb
-apt install -y ./zoxide_0.9.8-1_amd64.deb
-rm -rfv zoxide_0.9.8-1_amd64.deb
+#wget https://github.com/ajeetdsouza/zoxide/releases/download/v0.9.8/zoxide_0.9.8-1_amd64.deb
+#apt install -y ./zoxide_0.9.8-1_amd64.deb
+#rm -rfv zoxide_0.9.8-1_amd64.deb
 
 # Androidhoz
 # apt install -y adb fastboot android-file-transfer
