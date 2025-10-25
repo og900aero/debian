8a7c2ea93cbd0d07738704f661d318411b20eb4d
og900aero <49487376+og900aero@users.noreply.github.com>
2025-10-25 17:29:20 +0200

    Update install_wayland.sh

--- install_wayland.sh
+++ install_wayland.sh
@@ -29,7 +29,7 @@ apt install -y pipewire-audio pipewire-pulse wireplumber pavucontrol
 apt install -y xwayland swaylock dunst libnotify-bin rofi acpi brightnessctl clipman swaybg swayimg grim slurp wf-recorder
 
 # Fonts
-apt install -y fonts-font-awesome fonts-dejavu ttf-mscorefonts-installer
+apt install -y fonts-font-awesome fonts-dejavu ttf-mscorefonts-installer fonts-noto-color-emoji
 
 # File manager add-ons
 apt install -y unrar-free libfuse3-4 ifuse sshfs mediainfo zip unzip zstd 7zip poppler-utils ffmpegthumbnailer xlsx2csv bat catdoc docx2txt jq libimage-exiftool-perl chafa
